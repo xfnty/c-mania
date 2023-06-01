89a0f49f79bfbe34a0249bf020ec783dad80cd0a
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2023-06-01 18:33:17 +0700

    Removed skin related things

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -12,22 +12,11 @@
 
 #include <logging.h>
 #include <defines.h>
-#include <skin.h>
 
 
 int main(int argc, char const *argv[])
 {
     logging_init();
-
-    skin_t skin;
-    if (skin_load("./assets/skin.ini", &skin)) {
-        skin_debug_print(&skin);
-        skin_destroy(&skin);
-    }
-    else {
-        LOG("Failed");
-    }
-
     logging_shutdown();
     return 0;
 }
